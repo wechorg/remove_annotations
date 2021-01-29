09851687cd05fa3d5d9e69c9eebc50ab80baa164
Wechuli <wechulipaul@yahoo.com>
2021-01-29 14:23:15 +0300

    some changes

--- app.js
+++ app.js
@@ -7,3 +7,11 @@ class Person {
     console.log(`My name is ${this.first_name} ${this.last_name}`);
   }
 }
+
+function doSomething() {
+  let a = 90;
+  let b = 100;
+  for (let index = 0; index < 10; index++) {
+    console.log("Hi there");
+  }
+}
